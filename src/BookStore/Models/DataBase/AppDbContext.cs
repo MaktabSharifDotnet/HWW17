c8794df1652e1419f9dd543b245af2a474f44b63
aliizadjoo <Ali.zadjoo@gmail.com>
2025-10-28 07:33:03 +0330

    first commit

---
+++ src/BookStore/Models/DataBase/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Models.DataBase
+{
+    public class AppDbContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-M2BLLND\\SQLEXPRESS;Database=HWW17;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;");
+        }
+    }
+}
