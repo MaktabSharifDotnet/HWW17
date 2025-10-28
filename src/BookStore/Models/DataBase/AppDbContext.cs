df638b4634e776376bc59ca682e152a915848442
aliizadjoo <Ali.zadjoo@gmail.com>
2025-10-28 07:36:43 +0330

    add dbset

--- src/BookStore/Models/DataBase/AppDbContext.cs
+++ src/BookStore/Models/DataBase/AppDbContext.cs
@@ -1,9 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BookStore.Models.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace BookStore.Models.DataBase
 {
     public class AppDbContext:DbContext
     {
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=DESKTOP-M2BLLND\\SQLEXPRESS;Database=HWW17;Integrated Security=True;Encrypt=True;TrustServerCertificate=True;");
